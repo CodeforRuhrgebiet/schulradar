c5bce8ced2134421de92db4017d45f9c51366015
Martin Schurig <martin@schurig.pw>
2016-02-25 08:58:39 +0100

    fix paths everywhere

--- scripts/calc_coordinates.py
+++ scripts/calc_coordinates.py
@@ -2,7 +2,8 @@
 #-*- coding: utf-8 -*-
 
 import sys
-sys.path.insert(0, './GKConverter')
+import os
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + '/GKConverter')
 import gkconverter as GKConverter
 
 (x, y) = GKConverter.convert_GK_to_lat_long(float(sys.argv[1]), float(sys.argv[2]))
